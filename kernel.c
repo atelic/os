c03f8e71e3e1ebcc099ec9c3546c4b606d39aa28
Eric Barbour <emb4gu@virginia.edu>
2015-04-17 11:43:03 -0400

    Initialize terminal in kernel.c

---
+++ kernel.c
@@ -0,0 +1,72 @@
+#if !defined(__cplusplus)
+#include <stdbool.h> /* C doesn't have booleans by default. */
+#endif
+#include <stddef.h>
+#include <stdint.h>
+
+/* Check if the compiler thinks if we are targeting the wrong operating system. */
+#if defined(__linux__)
+#error "You are not using a cross-compiler, you will most certainly run into trouble"
+#endif
+ 
+/* This kernel will only work for the 32-bit ix86 targets. */
+#if !defined(__i386__)
+#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#endif
+
+/* Hardware text mode color constants. */
+enum vga_color
+{
+	COLOR_BLACK = 0,
+	COLOR_BLUE = 1,
+	COLOR_GREEN = 2,
+	COLOR_CYAN = 3,
+	COLOR_RED = 4,
+	COLOR_MAGENTA = 5,
+	COLOR_BROWN = 6,
+	COLOR_LIGHT_GREY = 7,
+	COLOR_DARK_GREY = 8,
+	COLOR_LIGHT_BLUE = 9,
+	COLOR_LIGHT_GREEN = 10,
+	COLOR_LIGHT_CYAN = 11,
+	COLOR_LIGHT_RED = 12,
+	COLOR_LIGHT_MAGENTA = 13,
+	COLOR_LIGHT_BROWN = 14,
+	COLOR_WHITE = 15,
+};
+
+uint8_t make_color(enum vga_color fg, enum vga_color bg)
+{
+	return fg | bg << 4;
+}
+
+uint16_t make_vgaentry(char c, uint8_t color)
+{
+	uint16_t c16 = c;
+	uint16_t colors16 = color;
+	return c16 | color16 << 8;
+}
+
+size_t strlen(const char* str)
+{
+	size_t ert = 0;
+	while(str[ret] != 0)
+		ret++
+	return ret;
+}
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
+
+void terminal_initialize()
+{
+	terminal_row = 0;
+	terminal_color = 0;
+	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+	
+}
